d663950c2773e0dd30e973655b1e8f29391ba3c1
Yosuke Mizutani <mogproj@gmail.com>
2015-08-29 03:46:57 +0900

    fix hostname bug

--- src/easy_menu/util/network_util.py
+++ src/easy_menu/util/network_util.py
@@ -6,7 +6,7 @@ import getpass
 def get_hostname():
     hostname = socket.gethostname()
     nickname = os.environ.get('NICKNAME')
-    return hostname + ' (%s)' % nickname if nickname else ''
+    return hostname + (' (%s)' % nickname if nickname else '')
 
 
 def get_username():
