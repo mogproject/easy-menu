d92ebb3f50f8949587522035c92b450bd8a2dbea
Yosuke Mizutani <mogproj@gmail.com>
2015-09-05 20:14:09 +0900

    add tests for logger closes #48

---
+++ tests/easy_menu/logger/test_logger.py
@@ -0,0 +1,17 @@
+from __future__ import division, print_function, absolute_import, unicode_literals
+
+from tests.universal import TestCase
+from tests.easy_menu.logger.mock_logger import MockLogger
+
+
+class TestLogger(TestCase):
+    def test_logger(self):
+        ml = MockLogger()
+        ml.info('Info message.')
+        ml.warn('Warn message.')
+        ml.error('Error message.')
+        self.assertEqual(ml.buffer, [
+            (6, '[INFO] Info message.'),
+            (4, '[WARN] Warn message.'),
+            (3, '[ERROR]Error message.'),
+        ])
