d4e4bc0fccb699199de302a5bfec76d7fbafd7ff
Yosuke Mizutani <mogproj@gmail.com>
2015-09-18 14:51:45 +0900

    add encoder to SystemLogger closes #57

--- src/easy_menu/util/string_util.py
+++ src/easy_menu/util/string_util.py
@@ -43,6 +43,25 @@ def to_unicode(s, encoding=None, errors='strict'):
             return str(s)
 
 
+def to_str(s, encoding=None, errors='strict'):
+    """
+    Make str from any value
+    :param s:
+    :param encoding:
+    :param errors:
+    :return: str (not unicode in Python2, nor bytes in Python3)
+    """
+    encoding = encoding or 'utf-8'
+
+    if is_strlike(s):
+        if six.PY2:
+            return s.encode(encoding, errors) if isinstance(s, unicode) else s
+        else:
+            return s.decode(encoding, errors) if isinstance(s, bytes) else s
+    else:
+        return str(s)
+
+
 def edge_just(left, right, width, fillchar=' '):
     padding = fillchar * max(1, width - unicode_width(left + right))
     return left + padding + right
--- tests/easy_menu/logger/test_system_logger.py
+++ tests/easy_menu/logger/test_system_logger.py
@@ -2,25 +2,21 @@
 from __future__ import division, print_function, absolute_import, unicode_literals
 
 import os
-from tests.universal import TestCase
+from tests.universal import TestCase, unittest
 from easy_menu.logger.system_logger import SystemLogger
 
 
 class TestSystemLogger(TestCase):
+    @unittest.skipUnless(os.environ.get('CI', '').lower() == 'true', 'run only in CI')
     def test_logger(self):
-        # should run only in CI
-        if os.environ.get('CI', '').lower() == 'true':
-            logger = SystemLogger()
+        logger = SystemLogger()
+        logger.info('Info message.')
+        logger.warn('Warn message.')
+        logger.error('Error message.')
 
-            logger.info('Info message.')
-            logger.warn('Warn message.')
-            logger.error('Error message.')
-
+    @unittest.skipUnless(os.environ.get('CI', '').lower() == 'true', 'run only in CI')
     def test_logger_unicode(self):
-        # should run only in CI
-        if os.environ.get('CI', '').lower() == 'true':
-            logger = SystemLogger()
-
-            logger.info('情報 メッセージ')
-            logger.warn('警戒 メッセージ')
-            logger.error('エラー メッセージ')
+        logger = SystemLogger()
+        logger.info('情報 メッセージ')
+        logger.warn('警戒 メッセージ')
+        logger.error('エラー メッセージ')
